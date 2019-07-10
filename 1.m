8ee8e50f403261071fbac7208f2d7b9106d3f99a
cgump <824592076@qq.com>
2019-07-10 11:34:24 +0800

    Initial commit

---
+++ 1.m
@@ -0,0 +1,21 @@
+function qianqian = feing(x)
+%feing - feijhe
+%
+% Syntax: qianqian = feing(x)
+%
+% fenge
+    for a = 1:10
+        if efe
+            fefd
+            fe
+        else
+            dfe
+            dfesr
+            da
+        else
+
+            dfef
+        end
+        
+    end
+end
